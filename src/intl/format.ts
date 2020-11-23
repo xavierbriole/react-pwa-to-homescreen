11067617b7f0eee0b13590bd8bbd56e0dac1aa94
xavierbriole <xbriole@icloud.com>
2020-11-23 14:07:53 +0100

    Add fallback i18n

--- src/intl/format.ts
+++ src/intl/format.ts
@@ -3,5 +3,9 @@ import messages from './messages'
 export const formatMessage = (id: string) => {
   const language = navigator.language.split('-')[0]
 
+  if (messages[language] === undefined) {
+    return messages.en[id]
+  }
+
   return messages[language][id]
 }
